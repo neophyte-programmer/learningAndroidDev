0df4a4f603e4bc0b598f9bc09192f97c02f442c6
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-15 14:43:33 +0000

    Update

--- Hello World/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Hello World/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+        Methods are fancy names for functions in Java
+        Methods should be created outside the scope of the onCreate method
+        But it should still be in the MainActivity class
+    */
+
+    public void onBtnClick (View view) {
+        // Assigning the textview text to a variable
+        // R stands for Resources and id is the type of resource then the textMessage is the name of
+        // id
+        TextView textHello = findViewById(R.id.textMessage);
+
+        /* Setting the text to something else */
+        textHello.setText("Hello People");
     }
 }
