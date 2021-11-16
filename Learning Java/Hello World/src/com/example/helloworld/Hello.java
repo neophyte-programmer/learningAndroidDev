788f2b97cf53611a139565c5fd5ecb87a01eefad
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-16 18:45:26 +0000

    Update

---
+++ Learning Java/Hello World/src/com/example/helloworld/Hello.java
@@ -0,0 +1,43 @@
+package com.example.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.print("Hello World");
+//        System.out.println("Hello World");
+
+//        int num = 7;
+//        System.out.println(num);
+//
+//        char myChar = '$';
+//        System.out.println(myChar);
+//
+//        boolean myBool = false;
+//        System.out.println(myBool);
+//
+//        double myDouble = 4.5;
+//        System.out.println(myDouble);
+//
+//        float myFloat = (float) 4.57;
+//        System.out.println(myFloat);
+//
+//        String name = "Nutifafa";
+//        System.out.println(name);
+
+        int a = 7;
+        int b = 4;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        float quotient = (float) a / b;
+        int remainder = a % b;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+        System.out.println(remainder);
+
+    }
+
+}
