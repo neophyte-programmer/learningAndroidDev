e55f34478b287dcef9a36db56f3a8124d0743c40
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-17 15:53:24 +0000

    Update

--- Learning Java/Operators/src/com/example/operators/Main.java
+++ Learning Java/Operators/src/com/example/operators/Main.java
@@ -12,9 +12,44 @@ public class Main {
 //        boolean answer = a==5 && b==2;
 //        System.out.println(answer);
 
-        if (a > b) {
-            System.out.println("A is greater than B");
+//        boolean answer = a > b;
+//
+//        if (!answer) {
+//            System.out.println("A is greater than B");
+//        } else {
+//            System.out.println("A is less than B");
+//        }
+//
+//        System.out.println("Continue ...");
+
+//        if (a > 0) {
+//            System.out.println("A is positive");
+//        } else if (a < 0) {
+//            System.out.println("A is negative");
+//        } else {
+//            System.out.println("A is 0");
+//        }
+
+        switch (b) {
+            case 1:
+                System.out.println("B is one");
+                break;
+            case 2:
+                System.out.println("B is two");
+                break;
+            case 3:
+                System.out.println("B is three");
+                break;
+            case 4:
+                System.out.println("B is four");
+                break;
+            default:
+                System.out.println("A is not 1 2 or 3");
+                break;
         }
+
+
+
 
     }
 }
