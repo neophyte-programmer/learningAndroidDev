020f2fca8a67279bba07eadbee1123d32e275ce7
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-17 14:05:53 +0000

    Update

---
+++ Learning Java/Operators/src/com/example/operators/Main.java
@@ -0,0 +1,20 @@
+package com.example.operators;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+//        boolean x = a > b;
+//
+//        System.out.println(x);
+
+//        boolean answer = a==5 && b==2;
+//        System.out.println(answer);
+
+        if (a > b) {
+            System.out.println("A is greater than B");
+        }
+
+    }
+}
