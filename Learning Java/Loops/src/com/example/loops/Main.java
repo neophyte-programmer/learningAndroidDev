cc4c46349a10711364b09b326ce3eae34843e700
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-11-18 18:40:25 +0000

    Update

---
+++ Learning Java/Loops/src/com/example/loops/Main.java
@@ -0,0 +1,42 @@
+package com.example.loops;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+
+//     for (int i = 0; i <10; i++) {
+//         System.out.println(i);
+//     }
+
+//     while (a < 10) {
+//         a++;
+//         if (a==8) {
+//             continue;
+//         }
+//         System.out.println(a);
+//
+//     }
+
+//        do {
+//            System.out.println("Hello");
+//        } while (a < 5);
+
+
+//        System.out.println("Please enter a number.");
+//        Scanner scanner = new Scanner(System.in);
+//        int answer = scanner.nextInt();
+//
+//        System.out.println("Answer was: " + answer);
+//
+//        System.out.println("Enter your name");
+//        String name = scanner.next();
+//
+//        System.out.println("Hello " + name);
+
+
+
+    }
+}
